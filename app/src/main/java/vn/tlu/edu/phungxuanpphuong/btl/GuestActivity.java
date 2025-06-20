60e6cc5868064e1251e35df021d3ca0217dad479
VTT <vthanhtu2510@gmail.com>
2025-06-20 19:31:16 +0700

    giao diện kh

--- app/src/main/java/vn/tlu/edu/phungxuanpphuong/btl/GuestActivity.java
+++ app/src/main/java/vn/tlu/edu/phungxuanpphuong/btl/GuestActivity.java
@@ -1,6 +1,26 @@
 package vn.tlu.edu.phungxuanpphuong.btl;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GuestActivity extends AppCompatActivity {
+
+    LinearLayout roomContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest); // Đảm bảo bạn có file này
+
+        roomContainer = findViewById(R.id.roomContainer); // ID này nằm trong activity_guest.xml
+
+    }
+
+
 }
+
