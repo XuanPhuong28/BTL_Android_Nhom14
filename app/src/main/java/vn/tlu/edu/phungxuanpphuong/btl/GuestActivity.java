e76ddb1ba65e2138e02692a16d52c01fe78e71d5
VTT <vthanhtu2510@gmail.com>
2025-06-21 11:30:53 +0700

    giao diện kh

--- app/src/main/java/vn/tlu/edu/phungxuanpphuong/btl/GuestActivity.java
+++ app/src/main/java/vn/tlu/edu/phungxuanpphuong/btl/GuestActivity.java
@@ -5,22 +5,64 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.tlu.edu.phungxuanpphuong.btl.cn2.RoomAdapter;
+import vn.tlu.edu.phungxuanpphuong.btl.cn2.RoomModel;
 
 public class GuestActivity extends AppCompatActivity {
-
-    LinearLayout roomContainer;
+    private RecyclerView recyclerView;
+    private RoomAdapter adapter;
+    private List<RoomModel> roomList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guest); // Đảm bảo bạn có file này
+        setContentView(R.layout.activity_guest); // chứa RecyclerView
 
-        roomContainer = findViewById(R.id.roomContainer); // ID này nằm trong activity_guest.xml
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        roomList = new ArrayList<>();
+        adapter = new RoomAdapter(roomList, this);
+        recyclerView.setAdapter(adapter);
+
+        DatabaseReference ref = FirebaseDatabase.getInstance("https://btlon-941fd-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("rooms");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                roomList.clear();
+                for (DataSnapshot snap : snapshot.getChildren()) {
+                    RoomModel room = snap.getValue(RoomModel.class);
+                    if (room != null) roomList.add(room);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(GuestActivity.this, "Lỗi tải dữ liệu!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-
 }
+
+
 
